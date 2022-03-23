243605f480bc7c9d77311291b556ad0204f07cb7
pizzmile <giacomo.pizza99@gmail.com>
2022-03-23 12:05:07 +0100

    Implementation of navigation scripts: - nav link update upon scroll - scroll to section upon click on nav link

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var navLinks = document.querySelectorAll("div.navbar ul.nav-list li.nav-item a.nav-link");
-var currentLink = navLinks[0];
-
-for (var i=0; i<navLinks.length; i++) {
-    navLinks[i].addEventListener("click", function () {
-        if (this !== currentLink) {
-            currentLink.classList.remove("current");
-            this.classList.add("current");
-            currentLink = this;
+// Update nav links upon scroll
+function updateCurrent(currentSectionId) {
+    $(".nav-link.current").removeClass("current");
+    $(".nav-link").each((index, element)=> {
+        if (element.getAttribute("href") === currentSectionId) {
+            element.classList.add("current");
+        } else {
+            element.classList.remove("current");
         }
     });
 }
+
+// Nav links click event
+$(".nav-link").click(function () {
+    if (!this.classList.contains("current")) {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(this.getAttribute("href")).offset().top
+        }, 500);
+    }
+});
+
+// Scroll event
+$(window).scroll(function (event) {
+    if ($(window).scrollTop() < $("#about").position()["top"]) {
+        updateCurrent("#home");
+    } else if ($(window).scrollTop() < $("#projects").position()["top"]) {
+        updateCurrent("#about");
+    } else if ($(window).scrollTop() < $("#contact").position()["top"]) {
+        updateCurrent("#projects");
+    } else {
+        updateCurrent("#contact");
+    }
+});
